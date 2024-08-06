65a183eeee6f5a7c0de7e50bfeed049dd2478813
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-06 10:02:58 +0300

    Proje dosyası ekle.

---
+++ patikakutuphane/Program.cs
@@ -0,0 +1,14 @@
+﻿using patikakutuphane;
+
+kitap kitap1 = new kitap();
+
+kitap1.adi = "Adı Aylin";
+kitap1.yazaradi = "Ayşe";
+kitap1.soyadi = "Kulin";
+kitap1.sayfasayisi = 698;
+kitap1.yayinevi = "Remzi Yayınevi";
+
+kitap kitap2 = new kitap("Dönüşüm", "Franz ", "Kafka", 598, "Can Yayınevi");
+
+Console.WriteLine(kitap1);
+Console.WriteLine(kitap2);
---
+++ patikakutuphane/kitap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patikakutuphane
+{
+    internal class kitap
+    {
+        public string adi {  get; set; }
+        public string yazaradi {  get; set; }
+        public string soyadi {  get; set; }
+        public int sayfasayisi { get; set; }
+        public string yayinevi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+
+        public kitap()
+        {
+            this.KayitTarihi = DateTime.Now;
+        }
+
+        public kitap(string ktpadi, string yad, string sad, int sayfa, string yevi)
+        {
+            this.adi = ktpadi;
+            this.yazaradi = yad;
+            this.soyadi = sad;
+            this.sayfasayisi = sayfa;
+            this.yayinevi = yevi;
+            this.KayitTarihi = DateTime.Now;
+
+        }
+        public override string ToString()
+        {
+            return $"Kitap Adı: {adi} İsim: {yazaradi} Soyisim: {soyadi} Sayfa Sayısı: {sayfasayisi} Yayın evi: {yayinevi} Kayıt tarihi: {KayitTarihi}";
+        }
+
+
+
+    }
+}
